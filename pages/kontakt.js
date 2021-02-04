7c115288bae6eb85a135070ba16c29c94b496f9e
Mateusz Jakowlew <mtjako@Mateuszs-Mac-mini.local>
2021-02-04 10:45:21 +0100

    bugfix

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -1,14 +1,11 @@
 import Head from "next/head";
-import { useState } from "react";
-import { Error } from "../components/Alerts/Error";
-import { Success } from "../components/Alerts/Success";
 import { Contact } from "../components/Contact";
 import { Faq } from "../components/Faq";
 import { Footer } from "../components/Footer";
 import { Navigation } from "../components/Navigation";
-import { getMenu, sendContact } from "../graphql/client";
+import { getMenu } from "../graphql/client";
 
-export async function getStaticProps(context) {
+export async function getStaticProps() {
   const DATA_MENU = await getMenu("mainmenu");
   const DATA_FOOTER = await getMenu("footer");
   const menu = DATA_MENU.menu.menuItems.nodes;
@@ -22,37 +19,7 @@ export async function getStaticProps(context) {
   };
 }
 
-export default function Post({ post, menu, footer }) {
-  // const [name, setName] = useState("");
-  // const [mail, setMail] = useState("");
-  // const [mess, setMess] = useState("");
-  // const [time, setTime] = useState(false);
-  // const [maxLength, setMaxLength] = useState(false);
-  // const [send, setSend] = useState(false);
-  // const [check, setCheck] = useState(false);
-  // function sendMessage(e) {
-  //   e.preventDefault();
-  //   setSend(false);
-  //   setTime(false);
-  //   setMaxLength(false);
-  //   if (mess.length <= 500) {
-  //     if (
-  //       parseInt(localStorage.getItem("time")) + 30000 < Date.now() ||
-  //       localStorage.getItem("time") == null
-  //     ) {
-  //       localStorage.setItem("time", Date.now());
-  //       sendContact(name, mail, mess);
-  //       setSend(true);
-  //     } else {
-  //       setTime(
-  //         30 -
-  //           parseInt(parseInt(Date.now() - localStorage.getItem("time")) / 1000)
-  //       );
-  //     }
-  //   } else {
-  //     setMaxLength(mess.length);
-  //   }
-  // }
+export default function Post({ menu, footer }) {
   return (
     <>
       <Head>
@@ -129,93 +96,7 @@ export default function Post({ post, menu, footer }) {
               fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
             />
           </svg>
-          <div>
-            {/*
-
-          <div className="text-center">
-            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              Formularz kontaktowy
-            </h2>
-            <p className="mt-4 text-lg leading-6 text-gray-500">
-              Skontaktuj się z proboszczem naszej parafii ks. Tomaszem Trębaczem
-            </p>
-          </div>
-          <div className="mt-12">
-            <form
-              action="#"
-              method="POST"
-              className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
-            >
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="company"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Imię i nazwisko
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    name="company"
-                    id="company"
-                    autoComplete="organization"
-                    className="py-3 px-4 block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                </div>
-              </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Email
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    className="py-3 px-4 block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
-                    onChange={(e) => setMail(e.target.value)}
-                  />
-                </div>
-              </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="message"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Wiadomość
-                </label>
-                <div className="mt-1">
-                  <textarea
-                    id="message"
-                    name="message"
-                    rows="4"
-                    className="py-3 px-4 block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
-                    onChange={(e) => setMess(e.target.value)}
-                  ></textarea>
-                </div>
-              </div>
-               <div className="sm:col-span-2">
-                <button
-                  type="submit"
-                  className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                  onClick={(e) => sendMessage(e)}
-                >
-                  Wyślij
-                </button>
-                {send ? <Success /> : null}
-                {time || maxLength ? (
-                  <Error time={time} length={maxLength} />
-                ) : null}
-              </div>
-            </form>
-          </div>*/}
-          </div>
-          <Contact/>
+          <Contact />
           <Faq />
         </div>
       </div>
