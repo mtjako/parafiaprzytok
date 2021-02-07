ebfad408ea6c5b5634df64a70677d019b427c61f
Mateusz Jakowlew <mtjako@Mateuszs-Mac-mini.local>
2021-02-07 14:40:52 +0100

    bugfix

--- components/Footer.js
+++ components/Footer.js
@@ -22,7 +22,8 @@ export const Footer = ({ footer }) => {
 
           </nav>
           <p className="mt-8 text-center text-base text-gray-500">
-            &copy; 2021 Parafia Przytok, All rights reserved.
+            &copy; 2021 Parafia Przytok | All rights reserved <br/>
+            Powered by <img src="/logodroszkowa.jpg"/> <b><span>D</span>roszków <span>O</span>nline</b> 
           </p>
         </div>
       </footer>
